7a79a55820d09d4123bede9a761d597bdf7bfbfd
Rishi <101804176+rishimakwana@users.noreply.github.com>
2025-03-18 17:16:05 +0530

    Add files via upload

---
+++ config/configuration.ts
@@ -0,0 +1,31 @@
+import * as dotenv from 'dotenv';
+
+export default () => {
+    const result = dotenv.config();
+    // const result = dotenv.config({
+    //     path: `.env.${process.env.NODE_ENV || 'dev'}`,
+    // });
+
+    if (result.error) {
+        throw new Error("Couldn't find .env file");
+    }
+    return {
+        db: {
+            database: process.env.DATABASE,
+            username: process.env.DB_USER,
+            dbPassword: process.env.DB_PASS,
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT)
+        },
+        emailCred: {
+            email: process.env.SEND_EMAIL,
+            password: process.env.EMAIL_PASSWORD,
+            back_office_email: process.env.BACK_OFFICE_EMAIL
+        },
+        common: {
+            port: process.env.PORT,
+            jwt: process.env.JWTKEY,
+            jwtExpire: process.env.TOKEN_EXPIRATION
+        },
+    };
+};
